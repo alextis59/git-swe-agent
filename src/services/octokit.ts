7c06406f56cca90398bf08211cfb2d79f7ae1d7a
Alex Tis <atissiere@traxxs.net>
2025-04-18 13:28:50 +0200

    fix for render

--- src/services/octokit.ts
+++ src/services/octokit.ts
@@ -1,8 +1,9 @@
 import { request } from "@octokit/request";
 import { createAppAuth } from "@octokit/auth-app";
 import { AppConfig } from "../types";
+import type { RequestInterface } from "@octokit/request";
 
-export async function createOctokitClient(config: AppConfig, installationId: number) {
+export async function createOctokitClient(config: AppConfig, installationId: number): Promise<RequestInterface> {
   return request.defaults({
     request: { 
       hook: createAppAuth({ 
